22971d295d884b35ee6d9d95138d2e188408aa37
Tyler Falgiano <tfalgiano@gmail.com>
2024-06-26 13:41:58 -0400

    added new lambda

--- aws/main.tf
+++ aws/main.tf
@@ -34,3 +34,17 @@ resource "aws_lambda_function" "my_hello_world" {
   }
 }
 
+resource "aws_lambda_function" "my_hello_world_2" {
+  runtime       = "nodejs12.x"
+  handler       = "exports.test"
+  image_uri     = "test"
+  function_name = "test"
+  role          = "arn:aws:ec2:us-east-1:123123123123:instance/i-1231231231"
+
+  memory_size = 512
+  tags = {
+    Environment = "Prod"
+  }
+}
+
+
